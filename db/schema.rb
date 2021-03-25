bb7ccb3f8cf00b76ac38fcfc1b3eb117a406c283
jssarrazolaa <jhonatan6512s.a@gmail.com>
2021-03-25 01:30:55 -0500

    Update column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_053049) do
+ActiveRecord::Schema.define(version: 2021_03_25_032931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_053049) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "user_id"
+    t.string "user_name"
     t.integer "restaurant_id"
     t.text "description"
     t.integer "score"
